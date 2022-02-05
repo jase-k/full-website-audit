9b63d9ec06e83ed22867ecb8038bbd4eab85cd3a
Jase Kraft <jase@perfectmypodcast.com>
2022-02-05 11:36:03 -0600

    added variables to the cli command

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ program
     .command('run')
     .description('audit your website with lighthouse')
     .option('-h, --host <string>', 'enter the host of the website you want to audit')
-    .option('-sd, --subdomains <string>', 'enter the path of a .txt file of the domains you want included in the web audit')
+    .option('-sd, --subdomainPath <string>', 'enter the path of a .txt file of the domains you want included in the web audit')
     .option('-l, --levels <integer>', 'enter the number of levels deep you want to crawl. (0= infinity; Default is 0)')
     .option('-e, --entrance <string>', 'enter the url of the page you want to start the crawl from')
     .action(webAudit)
